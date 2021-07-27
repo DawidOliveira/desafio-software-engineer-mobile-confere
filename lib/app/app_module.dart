ed7048db7885594e9599cafaba7debc33ac5093c
unknown <dawidoliveira1@gmail.com>
2021-07-27 12:57:03 -0300

    removendo páginas e imports desnecessários

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:product_manager/app/modules/add_product/add_product_module.dart';
-import 'package:product_manager/app/modules/product/product_module.dart';
 import 'package:product_manager/app/repositories/product/product_repository.dart';
 
 import 'modules/home/home_module.dart';
@@ -15,6 +14,5 @@ class AppModule extends Module {
   final List<ModularRoute> routes = [
     ModuleRoute(Modular.initialRoute, module: HomeModule()),
     ModuleRoute('/add-product', module: AddProductModule()),
-    ModuleRoute('/product', module: ProductModule()),
   ];
 }
